48657ed7bb06ee17fe8a14d9ce0fc4a321d402ab
henriquewallace <henrique.wallace@hotmail.com>
2025-03-31 19:25:10 -0300

    feat: initial commit

---
+++ src/main/java/com/henrique/meetime/MeetimeTestCaseApplication.java
@@ -0,0 +1,13 @@
+package com.henrique.meetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetimeTestCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetimeTestCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/henrique/meetime/MeetimeTestCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.meetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetimeTestCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
